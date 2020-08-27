89518381e16ed656aec4556065ca9d4804ccb8bb
Aurélien <orelienbertrand@gmail.com>
2020-08-27 19:29:00 +0200

    Updates

---
+++ precache-manifest.4456cadfba6dc0174d5e9534a6b1c969.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0e9c94b6bcb07b948ca10f3cba0fa0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5aee249475da2ce69b8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "64ea2dc1aae620a9ea4f",
+    "url": "/static/js/2.e9ec8aca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e9ec8aca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5aee249475da2ce69b8",
+    "url": "/static/js/main.57a6bc5a.chunk.js"
+  },
+  {
+    "revision": "7f2b423e5b99f007c593",
+    "url": "/static/js/runtime-main.62c936a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
