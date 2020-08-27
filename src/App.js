7fdc61c941a520d13bfb0f049cfd1957580166d1
Aurélien <orelienbertrand@gmail.com>
2020-08-27 20:28:21 +0200

    Coming Soon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LogoAMB from './images/LogoAMB.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="content">
+      <div class="content-bloc">
+        <img class="content-logo" src={LogoAMB} alt="Logo AMB"></img>
+        <div class="content-bloc-description">Bientôt de retour...</div>
+      </div>
     </div>
   );
 }
