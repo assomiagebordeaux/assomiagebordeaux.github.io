453d141abf45119baa46df24c2e107addb6f6f05
Aurélien <orelienbertrand@gmail.com>
2020-08-27 20:34:03 +0200

    change description coming soon

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div class="content">
       <div class="content-bloc">
         <img class="content-logo" src={LogoAMB} alt="Logo AMB"></img>
-        <div class="content-bloc-description">Bientôt de retour...</div>
+        <div class="content-bloc-description">Rendez-vous le 1er septembre...</div>
       </div>
     </div>
   );
