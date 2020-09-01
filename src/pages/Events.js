b6c836c4eaef958f2789d77eb2a03c3304ba39c1
Aurélien <orelienbertrand@gmail.com>
2020-09-01 10:41:11 +0200

    version-1-done

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -15,7 +15,7 @@ class Events extends Component {
                 type="event"
                 typeEvent="afterwork"
                 title="Afterwork AMC x JMC"
-                lieu="Lieu à venir"
+                lieu="Lieu à venir..."
                 date="mercredi 9 septembre 2020"
                 description="Venez découvrir les deux assos de la formation MIAGE : 
                 l'AMB, le Bureau des Elèves et JMC, la Junior-Entreprise."
@@ -29,7 +29,7 @@ class Events extends Component {
                 type="event"
                 typeEvent="afterwork"
                 title="Afterwork Parainnage"
-                lieu="Carnaval Café"
+                lieu="Lieu à venir..."
                 date="mercredi 16 septembre 2020"
                 description="Une soirée placée sous le signe des parrains. 
                 A vous de le débusquer !"
@@ -42,13 +42,27 @@ class Events extends Component {
                 type="event"
                 typeEvent="afterwork"
                 title="Afterwork Alumnis"
-                lieu="St-Aubin"
+                lieu="Lieu à venir..."
                 date="mercredi 23 septembre 2020"
                 description="Rencontrez les diplômés de MIAGE Bordeaux, 
                 que ce soit des M3 ou des M10 !"
                 button1Color="grey"
                 button1Label="Infos à venir"
                 button1Link="/events/aw-16092020"
+                isLast="true"
+                />
+                <Tuile 
+                display="true"
+                type="event"
+                typeEvent="unspecified"
+                title="WEI 2020"
+                lieu="Lieu à venir..."
+                date="du 3 au 4 octobre 2020"
+                description="Le WEI est bien maintenu, 
+                mais la vraie question est : quel sera le thème cette année ?"
+                button1Color="grey"
+                button1Label="Infos à venir"
+                button1Link="/events/wei-2020"
                 isLast="true"
                 />
                 {/* <div class="page-events-subtitle">Events passés</div> */}
